313a03194f1a2262fc001e1086d86f105e211b51
leenakhalili <engleena@yahoo.com>
2021-10-31 14:34:42 +0300

    House paint classes

--- HousPaint.py
+++ HousPaint.py
@@ -1,8 +1,12 @@
 class House:
     def __init__(self, wall_area):
         self.wall_area = wall_area
+    
+    def paint_needed(self):
+        return (self.wall_area * 2.5)
 
 class Paint:
-    def __init__(self, bucket, color):
-        self.bucket = bucket
+    def __init__(self, buckets, color):
+        self.buckets = buckets
         self.color = color
+
