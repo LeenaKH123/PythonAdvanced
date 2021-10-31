7cd448022d72d77014eac4e00bda7b90773b7aab
leenakhalili <engleena@yahoo.com>
2021-10-31 15:53:18 +0300

    completed goemetry game

--- GoemetryGameOOP.py
+++ GoemetryGameOOP.py
@@ -1,12 +1,13 @@
+from random import randint
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         # print(self)
 
-    def falls_in_rectangle(self, lowleft, upright):
-        if lowleft[0] < self.x < upright[0] \
-            and lowleft[1] < self.y < upright[1]:
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.point1.x < self.x < rectangle.point2.x \
+            and rectangle.point1.y < self.y < rectangle.point2.y:
             print("Im in the rectangle")
             # return True
         else:
@@ -17,14 +18,29 @@ class Point:
         # return ((self.x - x)**2 + (self.y - y)**2) ** 0.5
         print (((self.x - x)**2 + (self.y - y)**2) ** 0.5)
 
-# class Point:
-#     def __init__(self, x, y):
-#         this_object.x = x
-#         this_object.y = y
-#         print(self)
-        
+class Rectangle:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
 
-point1 = Point(3, 4)
-point1.distance_from_point(3,3)
-point2 = Point(6, 7)
-# type(point1)
+    def area(self):
+        return (self.point2.x - self.point1.x) * \
+            (self.point2.y - self.point1.y)
+
+# create rectangle object
+rectangle = Rectangle(Point(randint(0,9), randint(0,9)), Point(randint(10, 19), randint(10, 19)))
+
+# print rectangle coordinates
+print("Rectangle Coordinates: ",
+      rectangle.point1.x, ",",
+      rectangle.point1.y, "and",
+      rectangle.point2.x, ",",
+      rectangle.point2.y)
+
+# Get point and area from user
+user_point = Point(float(input("Guess x: ")), float(input("Guess y: ")))
+user_area = float(input("Guess rectangle area: "))
+
+# Print out the game result
+print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
+print("Your area was off by: ", rectangle.area() - user_area)
