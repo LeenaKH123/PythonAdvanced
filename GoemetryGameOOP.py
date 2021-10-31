bf214b67ff8e4cde59e62f501367eb3bce32ef7b
leenakhalili <engleena@yahoo.com>
2021-10-31 14:08:54 +0300

    Geometry game oop

--- GoemetryGameOOP.py
+++ GoemetryGameOOP.py
@@ -4,6 +4,15 @@ class Point:
         self.y = y
         # print(self)
 
+    def falls_in_rectangle(self, lowleft, upright):
+        if lowleft[0] < self.x < upright[0] \
+            and lowleft[1] < self.y < upright[1]:
+            print("Im in the rectangle")
+            return True
+        else:
+            print("Im not in the rectangle")
+            return False
+
 # class Point:
 #     def __init__(self, x, y):
 #         this_object.x = x
---
+++ HousPaint.py
@@ -0,0 +1,8 @@
+class House:
+    def __init__(self, wall_area):
+        self.wall_area = wall_area
+
+class Paint:
+    def __init__(self, bucket, color):
+        self.bucket = bucket
+        self.color = color
