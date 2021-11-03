b737daee08e7dce29c414e031a5e61f8b815a1ca
leenakhalili <engleena@yahoo.com>
2021-11-03 09:17:22 +0300

    installed Tkinter

---
+++ App1.py
@@ -0,0 +1,13 @@
+import turtle
+
+myturtle = turtle.Turtle()
+myturtle.forward(100)
+myturtle.left(90)
+turtle.done()
+
+# import turtle
+# test = turtle.Turtle()
+# test.color("orange")
+# test.pensize(5)
+# test.shape("turtle")
+# test.forward(100)
